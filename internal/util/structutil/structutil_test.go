375a2a6a6f83fa8414cb97d98da58c2ed01fec39
Hau Nguyen <thehaung217@gmail.com>
2023-03-26 12:36:00 +0700

    fix: ut

--- internal/util/structutil/structutil_test.go
+++ internal/util/structutil/structutil_test.go
@@ -21,9 +21,8 @@ func TestToJsonString(t *testing.T) {
 					Organization: "IT",
 				}
 
-				want := "{\n  \"id\": 0,\n  \"name\": \"Hau\",\n  \"team_name\": \"\",\n  \"organization\": \"IT\",\n  \"title\": \"SE\",\n  \"onboard_date\": \"0001-01-01T00:00:00Z\",\n  \"active\": false,\n  \"created_at\": \"0001-01-01T00:00:00Z\",\n  \"modified_at\": \"0001-01-01T00:00:00Z\"\n}"
+				want := "{\n  \"id\": 0,\n  \"name\": \"Hau\",\n  \"team_name\": \"\",\n  \"organization\": \"IT\",\n  \"title\": \"SE\",\n  \"onboard_date\": \"0001-01-01T00:00:00Z\",\n  \"active\": false,\n  \"created_at\": \"0001-01-01T00:00:00Z\",\n  \"modified_at\": \"0001-01-01T00:00:00Z\",\n  \"deleted_at\": \"0001-01-01T00:00:00Z\"\n}"
 				got := ToJsonString(structData)
-				fmt.Println(got)
 				require.Equal(t, want, got)
 			},
 		},
